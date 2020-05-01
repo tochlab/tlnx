d38c1a6ec95f57eccb7e6f88c605ceeec888b362
Dmitrii Tochanskii <tochansky@tochlab.net>
2020-05-01 21:46:03 +0300

    Install to /home/toch/tmp/

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 for s in $(find ./RPMS/ -name \*.rpm)
 do
 	echo ===== INSTALL $s to $1 =====
-	rpm -i --nodeps --nopost --root=$1 $s
+	rpm -i --nodeps --nopost --root=/home/toch/tmp/ $s
 	if [ $? -ne 0 ]; then
 		echo Problem with $s 
 		break;

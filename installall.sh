4ca7ddb282c83b3e1febdbcdac4e63b11b31da18
Dmitrii Tochanskii <tochansky@tochlab.net>
2022-10-02 17:04:58 +0300

    Update install.sh

--- installall.sh
+++ installall.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 INSTALL_DESTDIR=/home/tlnx/tmp/
+mkdir $INSTALL_DESTDIR/dev
+mkdir $INSTALL_DESTDIR/proc
+mount --bind /dev/ $INSTALL_DESTDIR/dev
+mount --bind /proc/ $INSTALL_DESTDIR/proc
 for s in $(find ./RPMS/ -name \*.rpm)
 do
 	echo ===== INSTALL $s to $INSTALL_DESTDIR =====
-	rpm --reinstall --nodeps --root=$INSTALL_DESTDIR $s
+	rpm --force --reinstall --nodeps --root=$INSTALL_DESTDIR $s
 	if [ $? -ne 0 ]; then
 		echo Problem with $s 
 		break;
